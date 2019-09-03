36a6dbdd999d79f916741d6a8917de3502424b2e
yangchch6 <yangchch6@yonyou.com>
2019-09-03 15:43:11 +0800

    publish 1.1.3

--- demo/demolist/Demo4.js
+++ demo/demolist/Demo4.js
@@ -18,7 +18,7 @@ class Demo4 extends Component {
         this.a = null;
     }
     getElement =()=>{
-        return document.getElementById('demo4')
+        return document.querySelector('.demo4')
     }
 
 
@@ -26,12 +26,6 @@ class Demo4 extends Component {
         return (
                 <div className="demo4">
                     <p>
-                        曾经有一份真诚的爱情放在我面前，我没有珍惜，等我失去的时候我才后悔莫及，人世间最痛苦的事莫过于此。
-                        如果上天能够给我一个再来一次的机会，我会对那个女孩子说三个字：我爱你。
-                        如果非要在这份爱上加上一个期限，我希望是……
-                        一万年
-                    </p>
-                    <p id="demo4">
                     
                         曾经有一份真诚的爱情放在我面前，我没有珍惜，等我失去的时候我才后悔莫及，人世间最痛苦的事莫过于此。
                         如果上天能够给我一个再来一次的机会，我会对那个女孩子说三个字：我爱你。
